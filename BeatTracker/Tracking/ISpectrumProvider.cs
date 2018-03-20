fbcd3075d7aca6ee1f996b13aea7b6bf8c2e70b2
Samuel Kupferschmid <samuel.kupferschmid@imnoo.com>
2018-03-21 00:27:30 +0100

    created simple spectrum form. closing #11

---
+++ BeatTracker/Tracking/ISpectrumProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatTracker.Tracking
+{
+    public interface ISpectrumProvider
+    {
+        IList<float[]> Samples { get; }
+
+        string Name { get; }
+
+        double SampleResolution { get; }
+        string SampleUnit { get; }
+
+        event EventHandler DataChanged;
+    }
+}
