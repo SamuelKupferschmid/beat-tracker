e7118f499017c6f18010d5ae2ffc7ed80f69755a
Samuel Kupferschmid <samuel.kupferschmid@imnoo.com>
2018-04-01 02:22:31 +0200

    WIP

--- BeatTracker.UI/SpectrumVisualization.cs
+++ BeatTracker.UI/SpectrumVisualization.cs
@@ -3,64 +3,155 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BeatTracker.Tracking;
+using BeatTracker.Utils;
 
 namespace BeatTracker.UI
 {
     public partial class SpectrumVisualization : Form
     {
-        private readonly ISpectrumProvider _provider;
-
-        public SpectrumVisualization(ISpectrumProvider provider)
+        public SpectrumVisualization()
         {
-            _provider = provider;
             InitializeComponent();
         }
 
         protected override void OnLoad(EventArgs e)
         {
-            this.Text = _provider.Name;
-
-            this._provider.DataChanged += UpdateData;
             this.canvas.Paint += UpdateDrawing;
-
+            Application.Idle += ApplicationOnIdle;
             base.OnLoad(e);
         }
 
-        private void UpdateData(object sender, EventArgs e)
+        private void ApplicationOnIdle(object sender, EventArgs eventArgs)
         {
             this.canvas.Invalidate();
+        }
+
+        private readonly Queue<float[]> _frames = new Queue<float[]>();
+        private const int SkipSize = 5;
+        private int _offset = 0;
+        private Bitmap _bitmap;
+
+        public void AddFrame(object sender, float[] frame)
+        {
+            var min = float.MaxValue;
+            var max = float.MinValue;
+
+            foreach (var v in frame)
+            {
+                if (v < min)
+                {
+                    min = v;
+                }
+
+                if (v > max)
+                {
+                    max = v;
+                }
+            }
+
+            if (min < MinValue)
+            {
+                MinValue = min;
+            }
+
+            if (max > MaxValue)
+            {
+                MaxValue = max;
+            }
+
+            lock (this)
+            {
+                _frames.Enqueue(frame);
+            }
         }
 
         private void UpdateDrawing(object sender, PaintEventArgs e)
         {
-            if (_provider.Samples.Count > 0)
+            lock (this)
             {
-                var width = _provider.Samples.Count;
-                var height = _provider.Samples[0].Length;
-                using (var img = new Bitmap(width, height))
+                while (_frames.Any())
                 {
-                    for (int x = 0; x < width; x++)
+                    var next = _frames.Dequeue();
+
+                    if (_bitmap == null)
                     {
-                        for (int y = 0; y < height; y++)
-                        {
-                            // TODO improve value -> color mapping
-                            img.SetPixel(x, y, Color.FromArgb((int) (_provider.Samples[x][y] * 255), Color.Blue));
-                        }
+                        _bitmap = new Bitmap(canvas.Width, next.Length);
                     }
 
-                    e.Graphics.DrawImage(img, e.ClipRectangle);
+                    if (_offset == canvas.Width)
+                    {
+                        using (var g = Graphics.FromImage(_bitmap))
+                        {
+                            g.DrawImage(_bitmap, -SkipSize, 0);
+                            g.FillRectangle(Brushes.White, _offset - SkipSize, 0, SkipSize, canvas.Height);
+                            g.Save();
+                        }
+
+                        _offset -= SkipSize;
+                    }
+
+                    for (int i = 0; i < next.Length; i++)
+                    {
+                        _bitmap.SetPixel(_offset, i, GetColor(next[i]));
+                    }
+
+                    _offset++;
                 }
             }
+
+            if (_bitmap != null) e.Graphics.DrawImage(_bitmap, 0, 0, canvas.Width, canvas.Height);
         }
 
-        private void UpdateDrawing(object sender, EventArgs e)
+        private Color GetColor(float value)
         {
-            this.canvas.Invalidate();
+            byte r = 0;
+            byte g = 0;
+            byte b = 0;
+
+            // 240° - 0° 
+
+            value = value.Clamp(MinValue, MaxValue);
+
+            var hue = (1 - ((MaxValue - MinValue) / (value - MinValue))) * 240 / 360;
+
+            float v2 = ((0.5f + 1) - (0.5f * 1));
+            float v1 = 2 * 0.5f - v2;
+
+            r = (byte) (255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+            g = (byte) (255 * HueToRGB(v1, v2, hue));
+            b = (byte) (255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+
+            return Color.FromArgb(255, r, g, b);
+        }
+
+        public float MaxValue { get; private set; }
+
+        public float MinValue { get; private set; }
+
+        private static float HueToRGB(float v1, float v2, float vH)
+        {
+            if (vH < 0)
+                vH += 1;
+
+            if (vH > 1)
+                vH -= 1;
+
+            if ((6 * vH) < 1)
+                return (v1 + (v2 - v1) * 6 * vH);
+
+            if ((2 * vH) < 1)
+                return v2;
+
+            if ((3 * vH) < 2)
+                return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+
+            return v1;
         }
     }
 }
