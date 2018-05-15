be6d2abc75301677886e0a01ba6e776505f2f921
Samuel Kupferschmid <samuel.kupferschmid@imnoo.com>
2018-05-15 21:01:19 +0200

    add resample

---
+++ BeatTracker.Test/Utils/SignalProcessingTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatTracker.Utils;
+using EasyAssertions;
+using Xunit;
+
+namespace BeatTracker.Test.Utils
+{
+    public class SignalProcessingTest
+    {
+        [Theory]
+        [InlineData(new float[] {1, 2, 3, 4, 5, 6}, 2, 3, new float[] {1f, 2.5f, 4f, 5.5f})]
+        [InlineData(new float[] {1, 2, 3, 4, 5, 6}, 1, 2, new float[] {1f, 3f, 5f})]
+        public void Resample(IEnumerable<float> source, int p, int q, float[] expected)
+        {
+            var result = SignalProcessing.Resample(source, p, q).ToArray();
+
+            result.Length.ShouldBe(expected.Count());
+        }
+
+        [Theory]
+        [InlineData(15, 5, 3, 1)]
+        [InlineData(2003, 80021, 2003, 80021)] // prime numbers
+        [InlineData(44100, 22050, 2, 1)]
+        public void TestRation(int a, int b, int expectedA, int expectedB)
+        {
+            var result = SignalProcessing.Ratio(a, b);
+            result.a.ShouldBe(expectedA);
+            result.b.ShouldBe(expectedB);
+        }
+    }
+}
---
+++ BeatTracker/Utils/SignalProcessing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatTracker.Utils
+{
+    public static class SignalProcessing
+    {
+        public static IEnumerable<float> Resample(IEnumerable<float> source, int p, int q)
+        {
+            (p, q) = Ratio(p, q);
+            int i = 0;
+            bool first = true;
+            float prev = 0;
+            foreach (var next in source)
+            {
+                if (first)
+                {
+                    prev = next;
+                    first = false;
+                    continue;
+                }
+
+                do
+                {
+                    if (i % q == 0)
+                    {
+                        float v = ((i % p) / (float)p);
+                        yield return prev + (next - prev) * v;
+                    }
+                } while (++i % p != 0);
+
+                prev = next;
+            }
+        }
+
+        public static (int a, int b) Ratio(int a, int b)
+        {
+            var gcd = (int)MathNet.Numerics.Euclid.GreatestCommonDivisor(a, b);
+            return (a / gcd, b / gcd);
+        }
+    }
+}
